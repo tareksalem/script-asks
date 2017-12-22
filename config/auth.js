448d71e71520f469e307af5e358f178b71467280
tareksalem <tarekpopp@gmail.com>
2017-12-23 01:33:57 +0200

    s

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     "facebookAuth": {
         "clientID": "199827757247091",
         "clientSecret": "4c5001024b09190d61790fe5f32b556f",
-        "callbackURL": "http://localhost:3000/users/auth/facebook/callback",
+        "callbackURL": "https://script-asks.herokuapp.com/users/auth/facebook/callback",
         profileFields: ["id", "email", "name", "gender", "displayName", "photos"],
         passReqToCallback: true
     }
